3fc3afc801a04c32799579a1dbb2c43be312383a
Tetyana Popenko <orkneya17@gmail.com>
2025-05-30 20:59:12 +0300

    clear

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -3,7 +3,7 @@ import { writeContacts } from '../utils/writeContacts.js';
 export const removeAllContacts = async () => {
   try {
     await writeContacts([]);
-    console.log('Усі контакти видалено!');
+    console.log('Контакти видалено!');
   } catch (err) {
     console.error('Помилка видалення контактів:', err);
   }
